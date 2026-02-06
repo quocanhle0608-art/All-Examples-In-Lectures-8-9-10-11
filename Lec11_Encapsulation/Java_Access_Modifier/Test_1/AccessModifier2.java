5d63008c86068764949ff273645574ad05afa1dd
quocanhle0608-art <quocanhle0608@gmail.com>
2026-02-06 12:02:21 +0700

    Add files via upload

---
+++ Lec11_Encapsulation/Java_Access_Modifier/Test_1/AccessModifier2.java
@@ -0,0 +1,12 @@
+package Encapsulation.Java_Access_Modifier.Test_1;
+
+public class AccessModifier2 {
+
+    public void display() {
+        AccessModifier1 object = new AccessModifier1();
+        System.out.println(object.pvt);
+        System.out.println(object.d);
+        System.out.println(object.pro);
+        System.out.println(object.pbl);
+    }
+}
