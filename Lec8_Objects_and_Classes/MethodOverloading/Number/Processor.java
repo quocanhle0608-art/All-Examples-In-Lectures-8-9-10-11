1e60c8cf41af4f44789ca123cd35f7f763e7a3df
quocanhle0608-art <quocanhle0608@gmail.com>
2026-02-06 09:22:08 +0700

    Add files via upload

---
+++ Lec8_Objects_and_Classes/MethodOverloading/Number/Processor.java
@@ -0,0 +1,9 @@
+package lec8_ObjectAndClass.MethodOverloading.Number;
+
+public class Processor {
+
+    public static void main(String[] args) {
+        System.out.println(OverloadingExample.add(1, 2));
+        System.out.println(OverloadingExample.add(1, 2, 3));
+    }
+}
