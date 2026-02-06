d536a51d3779a76178ebbf3239cf9bd9a6f415c4
quocanhle0608-art <quocanhle0608@gmail.com>
2026-02-06 12:06:02 +0700

    Add files via upload

---
+++ Lec11_Encapsulation/StaticKeyword/Static_Method/Restrictions/Processor.java
@@ -0,0 +1,11 @@
+package Encapsulation.StaticKeyword.Static_Method.Restrictions;
+
+public class Processor {
+
+    int number = 40;
+
+    public static void main(String args[]) {
+        Processor p = new Processor();   // tạo object
+        System.out.println(p.number);
+    }
+}
