666eeb2b168604c9e9cc10c91125d5bcbb628499
quocanhle0608-art <quocanhle0608@gmail.com>
2026-02-06 12:07:01 +0700

    Add files via upload

---
+++ Lec11_Encapsulation/StaticKeyword/Static_Variable/Person.java
@@ -0,0 +1,19 @@
+package Encapsulation.StaticKeyword.Static_Variable;
+
+public class Person {
+
+    private String name;
+    private int count = 0;
+
+    public Person(String n) {
+        name = n;
+        count++;
+        System.out.println(count);
+    }
+
+    public static void main(String[] args) {
+        Person p1 = new Person("Trang");
+        Person p2 = new Person("Duong");
+        Person p3 = new Person("Luong");
+    }
+}
